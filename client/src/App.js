55e2768281ee59606f44c0d5e9b3ffe6f07b0f7f
Chandu Kathi <chandukathi@csus.edu>
2024-02-28 09:29:14 -0800

    installed react-bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { Home } from "./pages/Home.js";
